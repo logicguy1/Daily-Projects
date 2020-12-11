f7278015b299ac4fe1b8975ef09f67d106264d84
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2020-12-11 08:49:47 +0100

    Update main.py

--- C*ns*r*d Str*ngs | Day 3/main.py
+++ C*ns*r*d Str*ngs | Day 3/main.py
@@ -1,10 +1,15 @@
-def uncensor(txt, vowels):
-  txtPack = txt.split("*")
+def uncensor(txt, vowels): # Initialise the function
+  out = "" # Out is used to store the final message
+  indx = 0 # Index is to keep track of what vowel we are on
   
-  out = ""
-  indx = 0
-  for i in txtPack:
-    out += f"{i}{vowels[indx]}"
-    indx += 1
-    
-  return out
+  for i in txt: # Loop over the message
+    if i == "*": # If the letter we are at is a star
+      out += vowels[indx] # give out the vowel 
+      indx += 1 # Increase index by one to get the next vowel next time
+      
+    else: # If the letter is a normal letter and not a "*"
+      out += i # Just give out the letter
+
+  return out # Return the result
+
+print(uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"))
