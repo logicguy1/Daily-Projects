42c09b401af61d50b24a1518bc8ed367ca880654
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2020-12-14 13:54:05 +0100

    Update main.py

--- ATM PIN Code Validation | Day 1/main.py
+++ ATM PIN Code Validation | Day 1/main.py
@@ -1,7 +1,6 @@
 def is_valid_PIN(pin): # Initalise the function
   if len(pin) in (4, 6) and pin.isnumeric(): # Check the lenght of the pin and if it is a number
     return True # Return true / false depending on the if statement
-  else:
-    return False
+  return False
 	
 print(is_valid_PIN("1943") # Run the function
